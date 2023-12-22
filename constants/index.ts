2d66800e1ede86df1dfe48122a4030658433cd98
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-22 23:07:54 +0500

    Exported Navbar component constant

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const navItems = [
+    {
+        id: 1,
+        title: "Home",
+        href: "home"
+    },
+    {
+        id: 2,
+        title: "About",
+        href: "about"
+    },
+    {
+        id: 3,
+        title: "Contact",
+        href: "contact"
+    },
+];
