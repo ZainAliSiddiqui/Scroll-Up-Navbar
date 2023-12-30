c784a138020a98a4cc9df76a82b81a986ee36fc8
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-30 15:20:11 +0500

    Used framer motion useMotion hook instead useEffect

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,27 +1,42 @@
-import { navItems } from "@constants";
+"use client";
 import Link from "next/link";
-import Header from "./Header";
+import { navItems } from "@constants";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
+import { useState } from "react";
+import { navVariants } from "@motion";
 
 export default function Navbar() {
+	const [hidden, setHidden] = useState(false);
+	const { scrollY } = useScroll();
+
+	useMotionValueEvent(scrollY, "change", (latest) => {
+		const previous = scrollY.getPrevious();
+		if (latest > previous) {
+			setHidden(true);
+		} else {
+			setHidden(false);
+		}
+	});
 	return (
 		<>
-			<Header>
-				<div className="w-full px-[100rem] text-black bg-white flex items-center justify-between h-[10vh]">
-					<div>
-						<h1 className="text-[30rem] font-bold tracking-wider">🌏 PLANET</h1>
-					</div>
-					<div className="flex items-center gap-x-[10rem]">
-						{navItems.map((item) => (
-							<Link
-								className="text-[22rem] font-medium tracking-wider cursor-pointer"
-								key={item.id}
-								href={item.href}>
-								{item.title}
-							</Link>
-						))}
-					</div>
+			<motion.nav
+				className="sticky top-0 w-full px-[100rem] text-black bg-white flex items-center justify-between h-[10vh] drop-shadow-lg"
+				variants={navVariants}
+				animate={hidden ? "hidden" : "vissible"}>
+				<div>
+					<h1 className="text-[30rem] font-bold tracking-wider">🌏 PLANET</h1>
 				</div>
-			</Header>
+				<div className="flex items-center gap-x-[10rem]">
+					{navItems.map((item) => (
+						<Link
+							className="text-[22rem] font-medium tracking-wider cursor-pointer"
+							key={item.id}
+							href={item.href}>
+							{item.title}
+						</Link>
+					))}
+				</div>
+			</motion.nav>
 		</>
 	);
 }
